e7482f0391484be18e2ba911286c13cf4a8545ac
kosunghun317 <kosunghun317@kaist.ac.kr>
2024-05-13 18:45:52 +0900

    feat: Added FHERC20 implementation and whitepaper

--- contracts/FugaziBase.sol
+++ contracts/FugaziBase.sol
@@ -3,11 +3,21 @@ pragma solidity >=0.8.13 <0.9.0;
 
 import "@fhenixprotocol/contracts/FHE.sol";
 import {Permissioned, Permission} from "@fhenixprotocol/contracts/access/Permissioned.sol";
+import {IFHERC20} from "./interfaces/IFHERC20.sol";
 
 // This contract is the base contract for all Fugazi contracts
 // It contains the basic data structures and storage variables
 // Lastly it contains account management functions
 contract FugaziBase is Permissioned {
+    using FHE for euint32;
+
+    // errors
+    error DepositFailed();
+    error WithdrawalFailed();
+
+    // events
+    event Deposit(address indexed recipient, address indexed token);
+
     // storage variables
     // more will be added as needed
     mapping(address => mapping(address => euint32)) internal accountTokenBalance;
@@ -15,7 +25,7 @@ contract FugaziBase is Permissioned {
     struct PoolStateStruct {
         address tokenX;
         address tokenY;
-        FugaziLPToken lpToken;
+        address lpToken;
         uint32 epoch;
         euint32 tokenXReserve;
         euint32 tokenYReserve;
@@ -26,8 +36,23 @@ contract FugaziBase is Permissioned {
     mapping(bytes32 => PoolStateStruct) public poolState;
 
     // deposit
+    function deposit(address recipient, address token, inEuint32 calldata _amount) external {
+        // transferFrom
+        euint32 spent = IFHERC20(token).transferFromEncrypted(msg.sender, address(this), _amount);
+
+        // update storage
+        accountTokenBalance[recipient][token] = accountTokenBalance[recipient][token] + spent;
+
+        // emit event
+        emit Deposit(recipient, token);
+    }
 
     // withdraw
+    function withdraw(address recipient, address token, inEuint32 calldata _amount) external {
+        // update storage
+
+        // transfer
+    }
 
     // check balance
 }
--- contracts/FugaziPoolRegistry.sol
+++ contracts/FugaziPoolRegistry.sol
@@ -5,7 +5,13 @@ import "contracts/FugaziBase.sol";
 import "contracts/FugaziLPToken.sol";
 
 contract FugaziPoolRegistry is FugaziBase {
+    // errors
+    error poolAlreadyExists();
+
     // create pool and update registry
+    function createPool(address tokenX, address tokenY, inEuint32 tokenReserves) external {
+        require(getPoolId(tokenX, tokenY) != bytes32(0), poolAlreadyExists());
+    }
 
     function getPoolId(address tokenX, address tokenY) public pure returns (bytes32) {
         return tokenX < tokenY ? keccak256(abi.encode(tokenX, tokenY)) : keccak256(abi.encode(tokenY, tokenX));
